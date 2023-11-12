8c51061b037b0e2cd1ef7d437c2dc58500a03c26
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2023-11-11 22:02:34 -0500

    Dar click a enlace

--- src/main/java/com/amazon/certificacion/calidad/interactions/AmazonHomePage.java
+++ src/main/java/com/amazon/certificacion/calidad/interactions/AmazonHomePage.java
@@ -4,13 +4,18 @@ import com.amazon.certificacion.calidad.userinterfaces.UsuarioPage;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Interaction;
 import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
 import org.openqa.selenium.Keys;
+
+import static com.amazon.certificacion.calidad.userinterfaces.UsuarioPage.LINK_HOMEPAGE;
+
 
 public class AmazonHomePage implements Interaction {
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(Enter.theValue("celular").into(UsuarioPage.INPUT_TEXT).thenHit(Keys.RETURN));
+        actor.attemptsTo(Click.on(LINK_HOMEPAGE));
     }
 
     public static AmazonHomePage go(){
--- src/main/java/com/amazon/certificacion/calidad/userinterfaces/UsuarioPage.java
+++ src/main/java/com/amazon/certificacion/calidad/userinterfaces/UsuarioPage.java
@@ -5,6 +5,6 @@ import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
 public class UsuarioPage extends PageObject {
-    public static final Target INPUT_TEXT = Target.the("INPUT_TEXT").located(By.id("APjFqb"));
-    public static final Target INPUT_TEXT1 = Target.the("INPUT_TEXT").located(By.id("//*[@id=\"twotabsearchtextbox\"]"));
+    public static final Target LINK_HOMEPAGE = Target.the("enlace celular").located(By.xpath("//*[@id=\'search\']/div[1]/div[1]/div/span[1]/div[1]/div[5]/div/div/div/div/div/div[1]/div/div[2]/div/span/a"));
+    public static final Target INPUT_TEXT = Target.the("INPUT_TEXT").located(By.id("twotabsearchtextbox"));
 }
